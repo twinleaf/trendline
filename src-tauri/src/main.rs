37a8f08d8bb85af8a4d5ddc0dc83a9440fb1d3e3
krzywonos <krzywonos@twinleaf.com>
2024-12-13 08:08:07 -0500

    Added on load/change updating on RPC commands

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,17 +1,58 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use tio::proto::DeviceRoute;
-use tio::proxy;
-use tio::util;
-use twinleaf::tio;
+use twinleaf::{tio, data::{ColumnData, Device}};
+use tio::{proto::DeviceRoute, proxy, util};
 use getopts::Options;
-use bytemuck::cast_slice;
-use twinleaf::data::{ColumnData, Device};
 
-use tauri::{Window, Emitter, Listener};
-use tauri::{LogicalPosition, LogicalSize, WebviewUrl};
-use std::time::Instant;
-use std::thread;
-use std::env;
+use tauri::{Emitter, Listener, LogicalPosition, LogicalSize, WebviewUrl, Window, Manager};
+use std::{env, thread};
+
+struct RpcMeta {
+    arg_type: String,
+    size: usize,
+    read: bool,
+    write: bool,
+    persistent: bool,
+    unknown: bool,
+}
+
+impl RpcMeta {
+    pub fn parse(meta: u16) -> RpcMeta {
+        let size = ((meta >> 4) & 0xF) as usize;
+        let atype = meta & 0xF;
+        RpcMeta {
+            arg_type: match atype {
+                0 => match size {
+                    1 => "u8",
+                    2 => "u16",
+                    4 => "u32",
+                    8 => "u64",
+                    _ => "",
+                },
+                1 => match size {
+                    1 => "i8",
+                    2 => "i16",
+                    4 => "i32",
+                    8 => "i64",
+                    _ => "",
+                },
+                2 => match size {
+                    4 => "f32",
+                    8 => "f64",
+                    _ => "",
+                },
+                3 => "string",
+                _ => "",
+            }
+            .to_string(),
+            size,
+            read: (meta & 0x0100) != 0,
+            write: (meta & 0x0200) != 0,
+            persistent: (meta & 0x0400) != 0,
+            unknown: meta == 0,
+        }
+    }
+}
+
 
 fn tio_opts() -> Options {
     let mut opts = Options::new();
@@ -50,152 +91,282 @@ fn tio_parseopts(opts: Options, args: &[String]) -> (getopts::Matches, String, D
     (matches, root, route)
 }
 
+fn get_rpctype(
+    name: &String,
+    device: &proxy::Port,
+) -> String {
+    RpcMeta::parse(device.rpc("rpc.info", name).unwrap()).arg_type
+}
+
+#[tauri::command]
+fn rpc(args: &[String]) -> std::io::Result<String> {
+    let mut opts = tio_opts();
+    opts.optopt(
+        "t",
+        "req-type",
+        "RPC request type (one of u8/u16/u32/u64 i8/i16/i32/i64 f32/f64 string). ",
+        "type",
+    );
+    opts.optopt(
+        "T",
+        "rep-type",
+        "RPC reply type (one of u8/u16/u32/u64 i8/i16/i32/i64 f32/f64 string). ",
+        "type",
+    );
+    opts.optflag("d", "", "Debug printouts.");
+    let (matches, root, route) = tio_parseopts(opts, args);
+
+    let rpc_name = if matches.free.is_empty() {
+        panic!("must specify rpc name")
+    } else {
+        matches.free[0].clone()
+    };
+
+    let rpc_arg = if matches.free.len() > 2 {
+        panic!("usage: name [arg]")
+    } else if matches.free.len() == 2 {
+        Some(matches.free[1].clone())
+    } else {
+        None
+    };
+
+    let debug = matches.opt_present("d");
+
+    let (status_send, proxy_status) = crossbeam::channel::bounded::<proxy::Event>(100);
+    let proxy = proxy::Interface::new_proxy(&root, None, Some(status_send));
+    let device = proxy.device_rpc(route).unwrap();
+    let mut result = "default".to_string() ;
+
+    let req_type = if let Some(req_type) = matches.opt_str("req-type") {
+        Some(req_type)
+    } else if rpc_arg.is_some() {
+        let t = get_rpctype(&rpc_name, &device);
+        Some(if t == "" { "string".to_string() } else { t })
+    } else {None};
+
+    let reply = match device.raw_rpc(
+        &rpc_name,
+        &if rpc_arg.is_none() {
+            vec![]
+        } else {
+            let s = rpc_arg.unwrap();
+            match &req_type.as_ref().unwrap()[..] {
+                "u8" => s.parse::<u8>().unwrap().to_le_bytes().to_vec(),
+                "u16" => s.parse::<u16>().unwrap().to_le_bytes().to_vec(),
+                "u32" => s.parse::<u32>().unwrap().to_le_bytes().to_vec(),
+                "u64" => s.parse::<u32>().unwrap().to_le_bytes().to_vec(),
+                "i8" => s.parse::<i8>().unwrap().to_le_bytes().to_vec(),
+                "i16" => s.parse::<i16>().unwrap().to_le_bytes().to_vec(),
+                "i32" => s.parse::<i32>().unwrap().to_le_bytes().to_vec(),
+                "i64" => s.parse::<i32>().unwrap().to_le_bytes().to_vec(),
+                "f32" => s.parse::<f32>().unwrap().to_le_bytes().to_vec(),
+                "f64" => s.parse::<f64>().unwrap().to_le_bytes().to_vec(),
+                "string" => s.as_bytes().to_vec(),
+                _ => panic!("Invalid type"),
+            }
+        },
+    ) {
+        Ok(rep) => rep,
+        Err(err) => {
+            if debug {
+                drop(proxy);
+                println!("RPC failed: {:?}", err);
+                for s in proxy_status.try_iter() {
+                    println!("{:?}", s);
+                }
+            }
+            return Err(std::io::Error::new(std::io::ErrorKind::Other, "RPC failed"));
+        }
+    };
+
+    if !reply.is_empty() {
+        let rep_type = if let Some(rep_type) = matches.opt_str("rep-type") {
+            Some(rep_type)
+        } else if req_type.is_none() {
+            let t = get_rpctype(&rpc_name, &device);
+            Some(if t == "" { "string".to_string() } else { t })
+        } else {req_type};
+    
+        let reply_str = match &rep_type.as_ref().unwrap()[..] {
+            "u8" => u8::from_le_bytes(reply[0..1].try_into().unwrap()).to_string(),
+            "u16" => u16::from_le_bytes(reply[0..2].try_into().unwrap()).to_string(),
+            "u32" => u32::from_le_bytes(reply[0..4].try_into().unwrap()).to_string(),
+            "u64" => u64::from_le_bytes(reply[0..8].try_into().unwrap()).to_string(),
+            "i8" => i8::from_le_bytes(reply[0..1].try_into().unwrap()).to_string(),
+            "i16" => i16::from_le_bytes(reply[0..2].try_into().unwrap()).to_string(),
+            "i32" => i32::from_le_bytes(reply[0..4].try_into().unwrap()).to_string(),
+            "i64" => i64::from_le_bytes(reply[0..8].try_into().unwrap()).to_string(),
+            "f32" => f32::from_le_bytes(reply[0..4].try_into().unwrap()).to_string(),
+            "f64" => f64::from_le_bytes(reply[0..8].try_into().unwrap()).to_string(),
+            "string" => format!(
+                "\"{}\" {:?}",
+                if let Ok(s) = std::str::from_utf8(&reply) {
+                    s
+                } else {
+                    ""
+                },
+                reply
+            ),
+            _ => panic!("Invalid type"),
+        };
+        result = reply_str.clone();
+        //println!("Reply: {}", reply_str.clone());
+    }
+    drop(proxy);
+    for s in proxy_status.iter() {
+        if debug {
+            println!("{:?}", s);
+        }
+    }
+    Ok(result)
+}
+
+
 #[tauri::command]
 fn graphs(window: Window) {
     let args: Vec<String> = env::args().collect();   
-    
     let opts = tio_opts();
     let (_matches, root, route) = tio_parseopts(opts, &args);
     
-    let proxy = proxy::Interface::new(&root);
+    thread::spawn(move || {  
+        let proxy = proxy::Interface::new(&root);
+        let device = proxy.device_full(route.clone()).unwrap();
+        let mut device = Device::new(device); 
+
+        //armstrong temp 
+        //TODO:: Need dynamic streams that will load in graphs in all instances (**targeting armstrong stream 1 field)
+        let mut current_name: String = String::new();
+        loop{
+            let sample = device.next();
+            let header = format!("Connected to: {}   Serial: {}   Session ID: {}", sample.device.name, sample.device.serial_number, sample.device.session_id);
+            let mut names: Vec<String> = Vec::new();
+            let mut values: Vec<f32> = Vec::new();
     
-    let device = proxy.device_full(route).unwrap();
-    //TODO: Fix hardcoded devname for two types of streaming ports
-    let devname: String = "USBSERIAL".to_string();
-    let mut device = Device::new(device);
-
-    let mut names: Vec<String> = Vec::new();
-    if devname.as_str() == "VMR" {
-        let column: String = device.get("data.stream.columns").unwrap(); // returns string of stream column names
-        for name in column.split_whitespace() {
-            names.push(name.to_string());
-        }
-    }
-    
-
-    thread::spawn(move || { 
-        //let (_tx, rx) = proxy.full_port().unwrap();
-        let start_time = Instant::now();
-        
-        match devname.as_str(){
-            "VMR" => {
-                for pkt in proxy.tree_full().unwrap().iter() {
-                    if let tio::proto::Payload::LegacyStreamData(ref data) = pkt.payload {
-                        let floats: &[f32] = cast_slice(&data.data);
-                        let elapsed = start_time.elapsed().as_secs_f32();
-
-                        let _ = window.emit("graphing", (&floats, &names, elapsed));
-                    }
-                }
-            }
-            //armstrong temp 
-            "USBSERIAL" => {
-                let mut current_name: String = String::new();
-                loop{
-                    let sample = device.next();
-                    let info = format!("Connected to: {}   Serial: {}   Session ID: {}", sample.device.name, sample.device.serial_number, sample.device.session_id);
-                    let mut names: Vec<String> = Vec::new();
-                    let mut values: Vec<f32> = Vec::new();
-            
-                    match sample.stream.stream_id{
-                        1 => {
-                            for column in &sample.columns{
-                                let name = column.desc.name.clone();
-                                if name != current_name && !(names.contains(&name)){
-                                    names.push(column.desc.name.clone());
-                                    current_name = name.clone();
-                                }
-                                values.push(match column.value {
-                                    ColumnData::Int(x) => x as f32,
-                                    ColumnData::UInt(x) => x as f32,
-                                    ColumnData::Float(x) => x as f32,
-                                    ColumnData::Unknown => 0.0,
-                                });
-                            }
-                            //let _ = window.emit("graphing", (&values, &names));
-
+            match sample.stream.stream_id{
+                1 => {
+                    for column in &sample.columns{
+                        let name = column.desc.name.clone();
+                        if name != current_name && !(names.contains(&name)){
+                            names.push(column.desc.name.clone());
+                            current_name = name.clone();
                         }
-                        0x02 => {
-                            for column in &sample.columns{
-                                names.push(column.desc.name.clone());
-                                values.push(match column.value {
-                                    ColumnData::Int(x) => x as f32,
-                                    ColumnData::UInt(x) => x as f32,
-                                    ColumnData::Float(x) => x as f32,
-                                    ColumnData::Unknown => 0.0,
-                                });
-                            }
-                            let _ = window.emit("graphing", (&values, &names, &info ));
-        
-                            
-                        }
-                        _ => {}
-   
-                    };
-                   
-                    for (name, value) in names.iter().zip(values.iter()){
-                        println!("{}: {}", name, value);
+                        values.push(match column.value {
+                            ColumnData::Int(x) => x as f32,
+                            ColumnData::UInt(x) => x as f32,
+                            ColumnData::Float(x) => x as f32,
+                            ColumnData::Unknown => 0.0,
+                        });
                     }
-                    //let _ = window.emit("graphing", (&values, &names));
+                    let _ = window.emit("field", (&values, &names, &header));
                 }
-                
-            }
-            &_ => {} 
-        }
-            
-    }); 
+                2 => {
+                    for column in &sample.columns{
+                        names.push(column.desc.name.clone());
+                        values.push(match column.value {
+                            ColumnData::Int(x) => x as f32,
+                            ColumnData::UInt(x) => x as f32,
+                            ColumnData::Float(x) => x as f32,
+                            ColumnData::Unknown => 0.0,
+                        });
+                    }
+                    let _ = window.emit("graphing", (&values, &names, &header));                         
+                }
+                _ => {}
+            };
+            /*for (name, value) in names.iter().zip(values.iter()){
+                println!("{}: {}", name, value);
+            }*/
+        }  
+
+    });
 }
-  
+
 fn main(){
     tauri::Builder::default()
         .setup(|app| {
-            //let window = app.get_webview_window("main").unwrap();
-
-            let window = tauri::window::WindowBuilder::new(app, "main")
+            let window = tauri::WindowBuilder::new(app, "main")
                 .inner_size(800., 600.)
+                .title("Lily")
                 .build()?;
 
-            let webview1 = window.add_child(
-                tauri::webview::WebviewBuilder::new("lily", WebviewUrl::App(Default::default()))
+            //webviews within main window
+            let aux = window.add_child(
+                tauri::webview::WebviewBuilder::new("aux", WebviewUrl::App(Default::default()))
                     .auto_resize(),
                     LogicalPosition::new(0., 0.),
                     LogicalSize::new(800., 600.),
                 )?;
-            
-            let webview2 = window.add_child(
-                tauri::webview::WebviewBuilder::new(
-                    "desc",
-                    WebviewUrl::App("desc.html".parse().unwrap()),
-                )
+
+            let desc = window.add_child(
+                tauri::webview::WebviewBuilder::new("desc", WebviewUrl::App("desc.html".parse().unwrap()),)
                 .auto_resize(),
                 LogicalPosition::new(0., 0.),
                 LogicalSize::new(800., 600.),
                 )?;
 
+            let field = tauri::WebviewWindowBuilder::new(app, "field", WebviewUrl::App("field.html".parse().unwrap()))
+                .title("Field")
+                .inner_size(750., 550.)
+                .build()?;
+
             //listen on backend for which webview to show
-            webview1.show().unwrap();
-            webview2.hide().unwrap();
+            aux.show().unwrap();
+            desc.hide().unwrap();
+            field.hide().unwrap();
 
             app.listen("toggle",  move |event| {
                 let webpage: String = serde_json::from_str(event.payload()).unwrap();
 
                 match webpage.as_str() {
                     "lily" => {
-                        webview1.show().unwrap();
-                        webview2.hide().unwrap();
+                        aux.show().unwrap();
+                        desc.hide().unwrap();
                     }
                     "desc" => {
-                        webview1.hide().unwrap();
-                        webview2.show().unwrap();
+                        aux.hide().unwrap();
+                        desc.show().unwrap();
+                    }
+                    "field" => {
+                        if field.is_visible().expect("Not visible") {
+                            field.hide().unwrap();
+                        } else {
+                            field.show().unwrap();
+                        }                        
                     }
                     _ => {}
                 }
+            }); 
 
-            });  
-            
+            let main_window = app.get_webview("aux").unwrap();
+            let new_window = app.get_webview("aux").unwrap();
+            app.listen("returningRPCName", move |event| {
+                let mut arg: Vec<String> = env::args().collect();   
+                _ = &arg.push("rpc".to_string());
+                let rpccall: Vec<String> = serde_json::from_str(event.payload()).unwrap();
+                let _ = &arg.push(rpccall[0].to_string());
+                let _ = &arg.push(rpccall[1].to_string());
+
+                if let Ok(passed) = rpc(&arg[2..]) {
+                    println!("Returning {} {}", rpccall[0].to_string(), passed.clone());
+                    let _ = main_window.emit("returnRPC", (rpccall[0].to_string(), passed.clone()));
+                }
+            });
+
+            app.listen("onLoad", move |event| {
+                let mut arg: Vec<String> = env::args().collect();   
+                _ = &arg.push("rpc".to_string());
+                let rpccall: String = serde_json::from_str(event.payload()).unwrap();
+                let _ = &arg.push(rpccall.to_string());
+
+                if let Ok(passed) = rpc(&arg[2..]) {
+                    let _ = new_window.emit("returnOnLoad", (rpccall.to_string(), passed.clone()));
+                }
+            });
+
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![graphs])
+        .invoke_handler(tauri::generate_handler![
+            graphs])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
     
--- src/uplot.js
+++ src/uplot.js
@@ -3,22 +3,23 @@ const { listen } = window.__TAURI__.event;
 const { getCurrentWebviewWindow } = window.__TAURI__.webviewWindow;
 
 invoke('graphs');
+//invoke('rpc_control')
 
 webpage = getCurrentWebviewWindow();
 
 window.onload = () => {
-
     var graphs = []; //store graphs for display
     var columns = []; //store names for canvases
-    var serial = [];
-    let points = 20;
-
+    var serial = []; //store device serial information
+    let timePoints = 10;
+    const inputChange = document.querySelectorAll('.InputCommands');
+    
     let startTime = Date.now();
-       
-    gotNames = false;
+    gotNames = false; //bool determines if graph information is set up
 
+    //emit rust data and push to uplot graphs 
     webpage.listen("graphing", (event) => {
-        const [values, name, info] = event.payload;
+        const [values, name, header] = event.payload;
         const elapsed = (Date.now() - startTime) /1000;
         
         //push names to canvas
@@ -26,7 +27,7 @@ window.onload = () => {
             for (let i = 0; i< name.length; i++) {
                 columns.push(name[i])
             }
-            serial.push(info)
+            serial.push(header)
             gotNames = true;
         } 
 
@@ -34,8 +35,8 @@ window.onload = () => {
             chart.data[0].push(elapsed)
             chart.data[1].push(values[index])
 
-            //TODO: Graph shifting as points filter in on change
-            let maxPoints = points;
+            //TODO: Fix graph shifting as points filter in on change (buggy)
+            let maxPoints = timePoints;
             if (chart.data[0].length > maxPoints){
                 chart.data[0].shift();
                 chart.data[1].shift();
@@ -46,6 +47,22 @@ window.onload = () => {
     });
 
     setTimeout(() => {
+        //display rpc values on load
+        inputChange.forEach(rpccall => {
+            webpage.emit('onLoad', rpccall.id);    
+        })
+
+        webpage.listen("returnOnLoad", (event) => {
+            let [name, inputValue] = event.payload;
+            inputChange.forEach(rpccall => {
+                if (rpccall.id == name) {
+                    rpccall.value = inputValue;
+                    rpccall.textContent = inputValue;
+                    rpccall.innerHTML = inputValue;
+                }
+            })
+        });
+
         //Push Sensor information to display
         const deviceinfo = document.getElementById('sensorinfo');
         const display = document.createElement('info');
@@ -56,20 +73,21 @@ window.onload = () => {
 
         //Iterate values for all charts 
         for (let i = 0; i < columns.length; i++) {
-            const checkboxesContainer = document.getElementById('please');
+            const checkboxesContainer = document.getElementById('dropdown');
             const canvasesContainer = document.getElementById('canvases');
 
-            //create check boxes general format 
+            //create checkboxes
             const checkbox = document.createElement('input');
             checkbox.type = "checkbox";
             checkbox.id = `canvasCheckbox${i}`;
             checkbox.className = 'checkboxes'
-            checkbox.value = i;
 
-            //create label for checkboxes
+            //create labels for checkboxes
             const label = document.createElement('label');
             label.htmlFor = checkbox.id;
             label.innerText = columns[i];
+
+            const lineBreak = document.createElement('br');
 
             //create canvas
             const canvas = document.createElement('div');
@@ -78,16 +96,22 @@ window.onload = () => {
             canvas.style.display = 'none';
             canvasesContainer.appendChild(canvas)
 
+            //add objects to div
             checkboxesContainer.appendChild(checkbox);
             checkboxesContainer.appendChild(label);
-
+            checkboxesContainer.appendChild(lineBreak);
             
-            //event listener to display canvas
+            //event listener to display canvas on click
             checkbox.addEventListener("change", (event) => {
                 const canvas = document.getElementById(`canvas${i}`)
                 canvas.style.display = event.target.checked ? 'block' : 'none';
-            });
 
+                document.querySelectorAll('.controls').forEach(rpcControl => {
+                    if (label.innerText.includes(rpcControl.id)){
+                        rpcControl.style.display = event.target.checked? 'inline-block' : 'none';
+                    }
+                });
+            });
 
             //uplot graph styling
             let options = {
@@ -101,23 +125,16 @@ window.onload = () => {
                         label: columns[i],
                         stroke: 'red',
                         points: { show: false },
-                        spanGaps: false,         
+                        spanGaps: true,         
     
                     },
-                ],
-                axes: [
-                    { },
-                    {
-                        tick: {show: true,},
-                        grid: {show: true}
-                    }
                 ],
                 scales: {
                     x: {
                     time: false,
                     distr: 2,
                     auto: true,
-                    range: [0,19]
+                    range: [0,9]
                     },
                 }
             }
@@ -127,6 +144,7 @@ window.onload = () => {
             const uplot = new uPlot(options, data, document.getElementById(canvas.id))
             graphs.push(uplot)
 
+            //interact js to resize charts
             const targetElement = document.getElementById(canvas.id)
             const targetResize = interact(targetElement);
 
@@ -155,6 +173,7 @@ window.onload = () => {
                     })
                 ]
             })
+
         }
 
     }, 1000);
@@ -162,41 +181,92 @@ window.onload = () => {
     //SIDE BAR LOGIC
     const drop = document.getElementById('drop')
     drop.addEventListener("click", function() {
-        const content = document.getElementById('please')
+        const content = document.getElementById('dropdown')
         content.classList.toggle("show");
     })
 
-    //page tabbing logic
+    //page tabbing
     document.querySelectorAll('.tabs div').forEach(tab => {
         tab.addEventListener('click', function() {
             //deactivate visibility
             document.querySelectorAll('.tabs div').forEach(tab => tab.classList.remove('active'));
-            
+
             //activate tab visibility
             tab.classList.add('active');
             
             const webviewShow = tab.id;
             webpage.emit('toggle', webviewShow)
-             
         })
     })
 
-    const timeSpan = document.getElementById('timeSpan');
-    timeSpan.addEventListener('input', () => {
-        const value = parseFloat(timeSpan.value);
-        const min = parseFloat(timeSpan.min)
-        const max = parseFloat(timeSpan.max)
-
-        if (isNaN(value) || value < min) {
-            points = min;
-        } else if (value > max) {
-            points = max;
-        } else {
-            points = value;
-        }
+    //on button click return rpc
+    const buttonChange = document.querySelectorAll('.buttonCommands');
+    buttonChange.forEach(clickButton => {
+        clickButton.addEventListener("click", function() {   
+            call = [clickButton.id, clickButton.value];     
+            webpage.emit('returningRPCName', call);      
+        })
     })
 
+    //on input change call rpc command
+    inputChange.forEach(rpccall => {
+        rpccall.addEventListener('keypress', function(e) {
+            value = in_range(rpccall).toString()
+            if (e.key == "Enter") {
+                //TODO: Determine whether to simply make the id the rpc name 
+                //or it needs to be something more private
+                call= [rpccall.id, value];
+                webpage.emit('returningRPCName', call);    
+            }
+        })
+    })
+
+    //on checkbox change call rpc command
+    const toggleChange = document.querySelectorAll('.checkCommands') 
+    toggleChange.forEach(clickToggle => {
+        clickToggle.addEventListener("click", function(e) {
+            if (e.checked) {
+                webpage.emit('returningRPC', clickToggle.id);
+            }
+        })
+    })
+
+
+    //returns all rpc values to corresponding input field
+    webpage.listen("returnRPC", (event) => {
+        let [name, inputValue] = event.payload;
+        inputChange.forEach(rpccall => {
+            if (rpccall.id == name) {
+                rpccall.value = inputValue;
+                rpccall.textContent = inputValue;
+                rpccall.innerHTML = inputValue;
+            }
+        })
+    });
+    
+    const timeSpan = document.getElementById('timeSpan');
+    timeSpan.addEventListener('keypress', function(e) {
+        if (e.key == "Enter") {
+            timePoints = in_range(timeSpan)
+        }
+    })
+    
 };
 
+//test if rpc input is within range
+function in_range(fillValue) {
+    const value = parseFloat(fillValue.value);
+    const min = parseFloat(fillValue.min)
+    const max = parseFloat(fillValue.max)
+
+    if (isNaN(value) || value < min) {
+        withinRange = min;
+    } else if (value > max) {
+        withinRange = max;
+    } else {
+        withinRange = value;
+    }
+    return withinRange
+}
 
     
