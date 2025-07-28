bef5cc280b2fed4b9a42de931b59a5b560d71ad8
Steven Nguyen <nguyen@twinleaf.com>
2025-07-28 16:53:28 -0400

    feature: Add placeholder panel components

---
+++ src/lib/components/stream-monitor/StreamMonitor.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<div class="w-full h-full flex flex-col space-y-2 rounded-lg border bg-card text-card-foreground p-4">
+	<h3 class="text-base font-semibold">Live Stream Values</h3>
+	<div class="flex-1 overflow-auto rounded-md border p-2">
+		<p class="pt-8 text-center text-sm text-muted-foreground">
+			This area will display a numeric view of selected streams and allow adding them to the chart.
+		</p>
+	</div>
+</div>
